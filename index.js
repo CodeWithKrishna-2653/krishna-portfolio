b4f910fa768eb65ea0ab6439ec0717696313da7b
NG KRISHNA <126246950+ng-krishna2653@users.noreply.github.com>
2023-10-21 14:31:44 +0530

    All File Are Modify By Marque Tool

--- index.js
+++ index.js
@@ -6,3 +6,34 @@ function displayTime() {
     document.getElementById('footer').innerHTML = time;
 }
 setInterval(displayTime, 1000);
+
+// Type write code
+
+const texts = ["teacher", "gym trainer", "web developer"];
+
+const typewriter = document.getElementById("dev-id");
+let textIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < texts[textIndex].length) {
+    typewriter.innerHTML += texts[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100); // Adjust typing speed here (in milliseconds)
+  } else {
+    setTimeout(erase, 1000); // Wait for a while before erasing
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typewriter.innerHTML = texts[textIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50); // Adjust erasing speed here (in milliseconds)
+  } else {
+    textIndex = (textIndex + 1) % texts.length;
+    setTimeout(type, 500); // Wait before typing the next line
+  }
+}
+
+type(); // Start the typing loop
